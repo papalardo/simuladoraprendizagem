e064edda2acb6b903d66d6ac38d0b5e61a9fe581
aluizafl <aluizafl@gmail.com>
2017-06-06 10:12:21 -0300

    Update curso.class.php

--- curso.class.php
+++ curso.class.php
@@ -1,7 +1,22 @@
 <?php
-class Curso {
+public class Curso {
 	private $id_cur;
 	private $nome_cur;
+	
+	public function __construct(){
+		$this->id_cur = 0;
+		$this->nome_cur = "";
+	}
+	
+	public function __construct($id_cur, $nome_cur){
+		$this->id_cur = $id_cur;
+		$this->nome_cur = $nome_cur;
+	}
+	
+	public function __construct($id_cur){
+		$this->id_cur = $id_cur;
+	}
+	
 	public function __set($atrib, $value){
 		$this->$atrib = $value;
 	}
