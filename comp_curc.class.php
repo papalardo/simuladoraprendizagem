6c37a2f23b7d576db51ee729f9b79933bd316395
aluizafl <aluizafl@gmail.com>
2017-06-06 10:19:10 -0300

    Update comp_curc.class.php

--- comp_curc.class.php
+++ comp_curc.class.php
@@ -1,30 +1,31 @@
 <?php
-class CompCurric {
-	private $idCcr;
-	private $nomeCcr;
-	private $cargaHorariaCcr;
+public class comp_curc {
+	private $id_ccr;
+	private $nome_ccr;
+	private $cargaHoraria_ccr;
 	
-	public function getIdCcr(){
-		return $this->idCcr;
+	public function __construct(){
+		$this->id_ccr = 0;
+		$this->nome_ccr = "";
+		$this->cargaHoraria_ccr = 0;
 	}
 	
-	public function getNomeCcr(){
-		return $this->nomeCcr;
+	public function __construct($id_ccr, $nome_ccr, $cargaHoraria_ccr){
+		$this->id_cur = $id_cur;
+		$this->nome_cur = $nome_cur;
+		$this->cargaHoraria_ccr = $cargaHoraria_ccr;
 	}
 	
-	public function getCargaHorariaCcr(){
-		return $this->cargaHorariaCcr;
+	public function __construct($id_ccr){
+		$this->id_ccr = $id_ccr;
 	}
 	
-	public function setIdCcr(){
-		return $this->idCcr;
+	public function __set($atrib, $value){
+		$this->$atrib = $value;
 	}
 	
-	public function setNomeCcr(){
-		return $this->nomeCcr;
+	public function __get($atrib){
+		return $this->$atrib;
 	}
 	
-	public function setCargaHorariaCcr(){
-		return $this->cargaHorariaCcr;
-	}
 }
