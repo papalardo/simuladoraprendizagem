56cc75d5ffe8723cd186bcf08113ddb516eadeb4
Glauco <glaucoteacher@gmail.com>
2017-06-12 22:14:08 -0300

    organizacao

--- view/perfil/Perfil.php
+++ view/perfil/Perfil.php
@@ -1,70 +1,18 @@
-<?php
-	function __autoload($class_name){
-		require_once 'models/' . strtolower($class_name) . '.class.php';
-	}
+<div class="col-md-6 col-md-offset-3">
+    <div class="panel panel-default">
+        <div class="panel-heading">Adicionar Perfil</div>
+        <div class="panel-body">
 
 
-$perfil = new Perfil();
-
-?>
-
-
-    <!DOCTYPE HTML>
-<?php include 'index.php'; ?>
-
-    <ul class="nav nav-tabs" role="tablist" id="myTab">
-        <li class="nav-item">
-            <a class="nav-link" href="?acao=adicionar" role="tab">Adicionar</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="?acao=listar" role="tab">Listar</a>
-        </li>
-    </ul>
-
-<br>
-<div class="container">
-<?php 
-        //Chamar paginas 
-        if(isset($_GET['acao']) && $_GET['acao'] == 'adicionar'){ include 'tpl/perfil/add.perfil.php'; }
-        if(isset($_GET['acao']) && $_GET['acao'] == 'editar'){ include 'tpl/perfil/edit.perfil.php';   } 
-        if(empty($_GET) || $_GET['acao'] == 'listar') { include 'tpl/perfil/list.perfil.php'; } 
-          
-         if(isset($_POST['cadastrar'])){
-
-            
-			$perfil->setDescricao($_POST['descricao']);
-
-			# Insert
-			if($perfil->adicionar()){
-				 echo "Cadastrado";
-			}
-
-		} # Termina Cadastro
-
-		
-		if(isset($_POST['atualizar'])){
-
-            
-			$perfil->setDescricao($_POST['descricao']);
-
-			if($perfil->atualizar($id)){
-				echo msg_erro("success","Feito!","Horario alterado com sucesso!");    
-			}
-
-		} #Termina Update
-		
-
-        if(isset($_GET['acao']) && $_GET['acao'] == 'deletar') {
-
-			$id = (int)$_GET['id'];
-			if($perfil->deletar($id)){
-                echo msg_erro("success","Feito!","Horario deletado!"); 
-				include 'tpl/horario/list.horario.php';
-                
-			}
-
-		}//Fecha Delete
-        
-		?>  
-    
-  </div>
+            <form action="" method="post" class="form-horizontal">
+                <div class="form-group">
+                    <label for="name" class="control-label col-sm-4">Descricao</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" name="descricao">
+                    </div>
+                </div>
+                <button type="submit" name="cadastrar" class="btn btn-default">Cadastrar</button>
+            </form>
+        </div>
+    </div>
+</div>
--- view/usuario/Usuario.php
+++ view/usuario/Usuario.php
@@ -1,75 +1,43 @@
-<?php
-	function __autoload($class_name){
-		require_once 'models/' . strtolower($class_name) . '.class.php';
-	}
+<div class="col-md-6 col-md-offset-3">
+    <div class="panel panel-default">
+        <div class="panel-heading">Adicionar Horario</div>
+        <div class="panel-body">
 
 
-$usuario = new Usuario();
-
-?>
-
-
-    <!DOCTYPE HTML>
-<?php include 'index.php'; ?>
-
-    <ul class="nav nav-tabs" role="tablist" id="myTab">
-        <li class="nav-item">
-            <a class="nav-link" href="?acao=adicionar" role="tab">Adicionar</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="?acao=listar" role="tab">Listar</a>
-        </li>
-    </ul>
-
-<br>
-<div class="container">
-<?php 
-        //Chamar paginas 
-        if(isset($_GET['acao']) && $_GET['acao'] == 'adicionar'){ include 'tpl/usuario/add.usuario.php'; }
-        if(isset($_GET['acao']) && $_GET['acao'] == 'editar'){ include 'tpl/usuario/edit.usuario.php';   } 
-        if(empty($_GET) || $_GET['acao'] == 'listar') { include 'tpl/usuario/list.usuario.php'; } 
-          
-
-        
-        if(isset($_POST['cadastrar'])){
-
-			$usuario->setNome($_POST['nome']);
-			$usuario->setEmail($_POST['email']);
-			$usuario->setCpf($_POST['cpf']);
-
-			# Insert
-			if($usuario->adicionar()){
-				 echo "Cadastrado";
-			}
-
-		} # Termina Cadastro
-
-		
-		if(isset($_POST['atualizar'])){
-
-			$usuario->setNome($_POST['nome']);
-			$usuario->setEmail($_POST['email']);
-			$usuario->setCpf($_POST['cpf']);
-            $id = $_GET['id'];
-            
-			if($usuario->atualizar($id)){
-				echo "Atualizado";  
-			}
-
-		} #Termina Update
-		
-
-        if(isset($_GET['acao']) && $_GET['acao'] == 'deletar') {
-
-			$id = (int)$_GET['id'];
-			if($usuario->deletar($id)){
-                echo msg_erro("success","Feito!","Horario deletado!"); 
-				include 'tpl/horario/list.horario.php';
+            <form action="" method="post" class="form-horizontal">
+                <div class="form-group">
+                    <label for="name" class="control-label col-sm-4">Nome</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" name="nome">
+                    </div>
+                </div>
                 
-			}
+                <div class="form-group">
+                    <label for="name" class="control-label col-sm-4">Email</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" name="email">
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <label for="name" class="control-label col-sm-4">CPF</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" name="cpf">
+                    </div>
+                </div>
 
-		}//Fecha Delete
-        
-		?>
-    
-  </div>
+                <div class="form-group">
+                    <label for="pwd" class="control-label col-sm-4">Avatar</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" name="avatar">
+                    </div>
+                </div>
+
+
+
+
+                <button type="submit" name="cadastrar" class="btn btn-default">Cadastrar</button>
+            </form>
+        </div>
+    </div>
+</div>
